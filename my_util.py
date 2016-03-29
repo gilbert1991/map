fc56458abf70bda9f37e8863f5be826ca117cd56
gilbert1991 <gilbert.lijie@gmail.com>
2016-03-29 15:53:00 -0400

    Update

--- my_util.py
+++ my_util.py
@@ -1,24 +1,18 @@
-import math
-from decimal import Decimal
+import geopy.distance as gpy
 
 # Convert geo coords (lat, lng) diff to distance in meters
-def geo2dist(origin, geo_diff):
-	distance = []
+# lat: 0.0001 ~= 11.1m, lng 0.0001 ~= 8.5m
+def geo_distance(ori, dst, method='vincenty'):
+	if method == 'vincenty':
+		distance = gpy.vincenty(ori, dst).meters
+	elif method == 'great_circle':
+		distance = gpy.great_circle(ori, dst).meters
+	else:
+		print('Error with geo_distance')
 
-	distance.append(40075160 * math.cos(math.radians(origin[0])))
-	distance.append(origin[1] * 40008000 / 360)
-	
-	return distance
+	return distance	
 
-# Convert distance diff (lat_meter, lng_meter) to geo coords
-def dist2geo(origin, dist_diff):
-	coords = []
-	coords.append(dist_diff[0] / (40075160.0 * math.cos(math.radians(origin[0])).real))
-	# coords.append(dist_diff[1] * 360 / 40008000)
-	coords.append(dist_diff[1] * 360.0 / 40008000.0)
-
-	return coords
-
-# def lat_circumference(lat):
-# 	lat_circum = lat * math.pi / 180
-# 	return lat_circum
+if __name__ == '__main__':
+	ori = (40.69465, -73.98327)
+	dst = (40.69465, -73.98337)
+	print(geo_distance(ori, dst, 'great_circle'))
--- test_script.py
+++ test_script.py
@@ -3,14 +3,21 @@ import file_handler
 import my_util
 
 # Identify image in a circle area centered at center(lat, lng) with radius(meter)
-# def find_in_range(image, center, radius):
+# interval(lat_interval, lng_interval)
+def find_in_range(center, radius, interval=(10), args):
+	sample_list = [center]
+
+	size = args.get('size', (800, 800))
+	fov = agrs.get('fov', 120)
+	heading = args.get('heading', [0, 90, 180, 270])
+	pitch = args.get('pitch', 10)
 
 
 
 if __name__ == '__main__':
-	encoded_args = http_handler.encode_args(location = [40.694305,-73.983298]);
-	file_path = "/Users/Gilbert/Documents/NYU/Master_Thesis/3D_Street_Navigator/image"
+	encoded_args = http_handler.encode_args(location = [40.694305,-73.983298], None);
+	file_path = "/Users/Gilbert/Documents/NYU/Master_Thesis/3D_Street_Navigator/image/test.jpeg"
 
-	# file_handler.img2file(http_handler.get_img(encoded_args), file_path)
+	file_handler.img2file(http_handler.get_img(encoded_args), file_path)
 	# print my_util.geo2dist([40.720032, -73.988354], [1, 1])
 	# print my_util.dist2geo([40.720032, -73.988354], [20, 20])
