149de084529c57f232005deaf42610b54e43130d
gilbert1991 <gilbert.lijie@gmail.com>
2016-03-30 13:53:56 -0400

    Third Commit

--- feature.py
+++ feature.py
@@ -1,1 +1,20 @@
+import cv2
+import numpy as np
+
 # Feature Extraction
+def sift_extraction(img_in, img_out):
+	img = cv2.imread(img_in)
+	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+	sift = cv2.SIFT()
+	kp = sift.detect(gray, None)
+
+	img = cv2.drawKeypoints(gray, kp, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+	cv2.imwrite(img_out, img)
+
+
+if __name__ == '__main__':
+	img = "/Users/Gilbert/Documents/NYU/Master_Thesis/3D_Street_Navigator/image/test.jpeg"
+	sift = "/Users/Gilbert/Documents/NYU/Master_Thesis/3D_Street_Navigator/image/test_sift.jpeg"
+	sift_extraction(img, sift)
--- test_script.py
+++ test_script.py
@@ -4,18 +4,18 @@ import my_util
 
 # Identify image in a circle area centered at center(lat, lng) with radius(meter)
 # interval(lat_interval, lng_interval)
-def find_in_range(center, radius, interval=(10), args):
-	sample_list = [center]
+# def find_in_range(center, radius, interval=(10), args):
+# 	sample_list = [center]
 
-	size = args.get('size', (800, 800))
-	fov = agrs.get('fov', 120)
-	heading = args.get('heading', [0, 90, 180, 270])
-	pitch = args.get('pitch', 10)
+# 	size = args.get('size', (800, 800))
+# 	fov = agrs.get('fov', 120)
+# 	heading = args.get('heading', [0, 90, 180, 270])
+# 	pitch = args.get('pitch', 10)
 
 
 
 if __name__ == '__main__':
-	encoded_args = http_handler.encode_args(location = [40.694305,-73.983298], None);
+	encoded_args = http_handler.encode_args(location = [40.694305,-73.983298]);
 	file_path = "/Users/Gilbert/Documents/NYU/Master_Thesis/3D_Street_Navigator/image/test.jpeg"
 
 	file_handler.img2file(http_handler.get_img(encoded_args), file_path)
